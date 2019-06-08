33a2a8272ca9a1e2b4bdadabf2d8726db9621d52
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-08 13:06:22 -0400

    Setup for StarWarsCharacter

---
+++ starwars/src/components/StarWarsCharacter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Have at least one list element for each star wars character in the data set.
+
+function StarWarsCharacter(props) {
+
+    const clickHandler = () => {
+        // I'd like a clickHandler when clicked pulls up more data... 
+    };
+
+    return (
+        <li onClick={clickHandler} >
+            {props.character.name}
+        </li>
+    );
+}
+export default StarWarsCharacter;
