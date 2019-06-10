12a228783376bc2abf86fb8d4ba0fbc01a400a89
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-10 00:06:04 -0400

    Setup right and left arrows and blank click handlers

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -4,14 +4,21 @@ import StarWarsCharacter from './StarWarsCharacter';
 // A list of Star Wars Characters rendered to the screen.
     
 function StarWarsList(props) {
+
+    const pageHandlerLeft = () => { };
+
+    const pageHandlerRight = () => { };
+
     return (
         <div className="list">
+            <div className="left arrow" onClick={pageHandlerLeft} >&laquo;</div>
             <ul>
                 {props.list.map( item => {
                     return (
                     <StarWarsCharacter character={item} key={item.url} getCharacterData={props.getCharacterData} />
                 );})}
             </ul>
+            <div className="right arrow" onClick={pageHandlerRight} >&raquo;</div>
         </div>
     );
 }
